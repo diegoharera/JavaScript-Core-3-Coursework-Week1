e5776b2b6fc395edcca3baaf7b641e673f24d09e
Diego Harera <104278852+diegoharera@users.noreply.github.com>
2022-10-23 11:20:26 -1100

    Add files via upload

--- mandatory/debugging-practice/script.js
+++ mandatory/debugging-practice/script.js
@@ -14,8 +14,10 @@ function populateStorage() {
       "127",
       true
     );
+    let book3 = new Book("Robison Crusoe2", "Daniel Defoe", "252", false)
     myLibrary.push(book1);
     myLibrary.push(book2);
+    myLibrary.push(book3);
     render();
   }
 }
@@ -38,7 +40,7 @@ function submit() {
     return false;
   } else {
     let book = new Book(title.value, title.value, pages.value, check.checked);
-    library.push(book);
+    myLibrary.push(book);
     render();
   }
 }
@@ -54,7 +56,7 @@ function render() {
   let table = document.getElementById("display");
   let rowsNumber = table.rows.length;
   //delete old table
-  for (let n = rowsNumber - 1; n > 0; n-- {
+  for (let n = rowsNumber - 1; n > 0; n--) {
     table.deleteRow(n);
   }
   //insert updated row and cells
@@ -90,11 +92,12 @@ function render() {
 
     //add delete button to every row and render again
     let delButton = document.createElement("button");
-    delBut.id = i + 5;
-    cell5.appendChild(delBut);
-    delBut.className = "btn btn-warning";
-    delBut.innerHTML = "Delete";
-    delBut.addEventListener("clicks", function () {
+    delButton.id = i + 5;
+    cell5.appendChild(delButton);
+    delButton.className = "btn btn-warning";
+    delButton.innerHTML = "Delete";
+    delButton.addEventListener("clicks", (e) => {
+      console.log(e.target)
       alert(`You've deleted title: ${myLibrary[i].title}`);
       myLibrary.splice(i, 1);
       render();
